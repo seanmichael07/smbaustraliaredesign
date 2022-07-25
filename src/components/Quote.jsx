3709f83939eac4b0e7918ca00309add26ae1dd54
Sean Burrows <seanmichael07@hotmail.com>
2022-07-25 23:18:43 +1000

    WIP - working on quotes page

--- src/components/Quote.jsx
+++ src/components/Quote.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Nav from './Nav'
+import QuoteForm from './QuoteFormComponents/QuoteForm'
 
 export default function Quote() {
     return (
-        <div>
+        <div className="border border-black h-screen">
             <Nav />
-            <h1>Quotes Page</h1>
+            <div className="h-full relative border border-black">
+                <div className="min-h-[45%] bg-orange" />
+                <div className="quoteContainer absolute top-1/3 right-1/2 bg-white border border-black">
+                    <QuoteForm />
+                </div>
+            </div>
         </div>
     )
 }
---
+++ src/components/QuoteFormComponents/QuoteForm.jsx
@@ -0,0 +1,101 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react'
+import DestinationDetails from './DestinationDetails'
+import CarDetails from './CarDetails'
+import CustomerDetails from './CustomerDetails'
+import Success from './Success'
+import Confirmation from './Confirmation'
+
+export default class QuoteForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            step: 1,
+            origin: '',
+            destination: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+            make: '',
+            model: '',
+            year: '',
+            isDriveable: true,
+        }
+    }
+
+    // go back to previous step
+    prevStep = () => {
+        const { step } = this.state
+        this.setState({ step: step - 1 })
+    }
+
+    // proceed to next step
+    nextStep = () => {
+        const { step } = this.state
+        this.setState({ step: step + 1 })
+    }
+
+    // handle field change
+    handleChange = (input) => (e) => {
+        this.setState({ [input]: e.target.value })
+    }
+
+    render() {
+        const { step } = this.state
+        const { origin, destination, firstName, lastName, email, make, model, year, isDriveable } =
+            this.state
+        const values = {
+            origin,
+            destination,
+            firstName,
+            lastName,
+            email,
+            make,
+            model,
+            year,
+            isDriveable,
+        }
+        switch (step) {
+            case 1:
+                return (
+                    <DestinationDetails
+                        nextStep={this.nextStep}
+                        handleChange={this.handleChange}
+                        values={values}
+                    />
+                )
+            case 2:
+                return (
+                    <CarDetails
+                        prevStep={this.prevStep}
+                        nextStep={this.nextStep}
+                        handleChange={this.handleChange}
+                        values={values}
+                    />
+                )
+
+            case 3:
+                return (
+                    <CustomerDetails
+                        prevStep={this.prevStep}
+                        nextStep={this.nextStep}
+                        handleChange={this.handleChange}
+                        values={values}
+                    />
+                )
+
+            case 4:
+                return (
+                    <Confirmation
+                        prevStep={this.prevStep}
+                        nextStep={this.nextStep}
+                        values={values}
+                    />
+                )
+            case 5:
+                return <Success />
+            default:
+            // do nothing
+        }
+    }
+}
