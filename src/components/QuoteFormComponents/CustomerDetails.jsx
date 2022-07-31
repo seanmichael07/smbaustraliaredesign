4f2f14788014fe4fa8511e78a5ab24ad6e047bc0
Sean Burrows <seanmichael07@hotmail.com>
2022-07-31 22:45:04 +1000

    WIP - Working on quote form

--- src/components/QuoteFormComponents/CustomerDetails.jsx
+++ src/components/QuoteFormComponents/CustomerDetails.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
 
-function CustomerDetails() {
-    return <div>CustomerDetails</div>
+function CustomerDetails({ prevStep, nextStep, handleChange, values }) {
+    const Continue = (e) => {
+        e.preventDefault()
+        nextStep()
+    }
+
+    const Previous = (e) => {
+        e.preventDefault()
+        prevStep()
+    }
+
+    return (
+        <div>
+            <form>
+                <label htmlFor="firstName">
+                    First Name
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        label="First Name"
+                        onChange={handleChange('firstName')}
+                        defaultValue={values.firstName}
+                    />
+                </label>
+                <br />
+                <button onClick={Previous} type="submit">
+                    Previous
+                </button>
+                <button onClick={Continue} type="submit">
+                    Next
+                </button>
+            </form>
+        </div>
+    )
 }
 
 export default CustomerDetails
