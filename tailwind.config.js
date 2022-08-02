46c6fa9ce32f13ed673d43b3ad3acaf9d1a83a24
Sean Burrows <seanmichael07@hotmail.com>
2022-08-03 00:43:09 +1000

    Part of adding fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     content: ['./src/**/*.{html,js,jsx}'],
     theme: {
+        fontFamily: {
+            sans: ['Montserrat', 'sans-serif'],
+        },
         extend: {
             colors: {
                 orange: '#ff7100',
